5501d757bd69e01d33c93cb43e207b864f4f5d39
Ninorokr <54788051+Ninorokr@users.noreply.github.com>
2021-08-20 17:47:04 -0500

    Add files via upload

--- CopiarArchivosDesdeTxt/src/com/alvarogiron/Main.java
+++ CopiarArchivosDesdeTxt/src/com/alvarogiron/Main.java
@@ -9,23 +9,27 @@ import java.util.Scanner;
 
 public class Main {
 
-    static ArrayList<String> rutas = new ArrayList<>();
-    static ArrayList<String> nomArchivo = new ArrayList<>();
+    static ArrayList<String> contrastes = new ArrayList<>();
+    static ArrayList<String> suministros = new ArrayList<>();
 
     public static void main(String[] args) {
         try {
-            Scanner scanner = new Scanner(new File("D:/cargos.txt"));
-            Scanner scanner2 = new Scanner(new File("D:/nrosCaso.txt"));
+            Scanner scanner = new Scanner(new File("D:\\contrastes.txt"));
+            Scanner scanner2 = new Scanner(new File("D:\\suministros.txt"));
             while (scanner.hasNextLine()) {
                 //System.out.println(scanner.nextLine());
-                rutas.add(scanner.nextLine());
-                nomArchivo.add(scanner2.nextLine());
+                contrastes.add(scanner.nextLine());
+                suministros.add(scanner2.nextLine());
             }
             scanner.close();
-            for(int i = 0; i < rutas.size(); i++) {
+            scanner2.close();
+            for(int i = 0; i < contrastes.size(); i++) {
+                String contraste = contrastes.get(i);
+                String OS = contraste.substring(0,7);
+                String ruta = "\\\\192.168.1.2\\img\\2021\\JUN\\ENDESA\\33333333333\\" + OS + "\\" + contraste + ".gif";
                 try {
-                    copyFileUsingJava(new File(rutas.get(i)), new File("D:/Solicitados2/" + nomArchivo.get(i) + ".pdf"));
-                    System.out.println(i+1 + ". Se copió " + nomArchivo.get(i) + ".pdf en D:/Solicitados2/");
+                    copyFileUsingJava(new File(ruta), new File("D:/Muestra/SUM " + suministros.get(i) + ".gif"));
+                    System.out.println(i+1 + ". Se copió " + contraste + ".gif en D:/Muestra/");
                 } catch (IOException ex) {
                     System.out.println(ex.toString());
                 }
